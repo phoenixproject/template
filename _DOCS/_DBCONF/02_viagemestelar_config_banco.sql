652a5839204d728330bb4fbe4af820529f2593a0
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-20 09:33:44 -0300

    Alteração pontuais no banco

--- _DOCS/_DBCONF/02_viagemestelar_config_banco.sql
+++ _DOCS/_DBCONF/02_viagemestelar_config_banco.sql
@@ -1,23 +1,23 @@
---Tabela Tipo_Usuario
-insert into Tipo_Usuario values('','Admin')
-insert into Tipo_Usuario values('','Usuário')
+-- Tabela Tipo_Usuario
+insert into Tipo_Usuario values('','Admin');
+insert into Tipo_Usuario values('','Usuário');
 
---Tabela Tipo_Nave
-insert into Tipo_Nave values('','Stardust (E27-01)')
-insert into Tipo_Nave values('','Space Discovery (ASX-99)')
-insert into Tipo_Nave values('','Super Estellar (D3R-777)')
+-- Tabela Tipo_Nave
+insert into Tipo_Nave values('','Stardust (E27-01)');
+insert into Tipo_Nave values('','Space Discovery (ASX-99)');
+insert into Tipo_Nave values('','Super Estellar (D3R-777)');
 
---Tabela Tipo_Arma
-insert into Tipo_Arma values('','Lasershot')
-insert into Tipo_Arma values('','Gun Bomb')
-insert into Tipo_Arma values('','Flash Machine Gun')
+-- Tabela Tipo_Arma
+insert into Tipo_Arma values('','Lasershot');
+insert into Tipo_Arma values('','Gun Bomb');
+insert into Tipo_Arma values('','Flash Machine Gun');
 
---Tabela Tipo_Atividade
-insert into Tipo_Atividade values('','Login em Cliente')
-insert into Tipo_Atividade values('','Login no Servidor')
-insert into Tipo_Atividade values('','Pontução Básica')
+-- Tabela Tipo_Atividade
+insert into Tipo_Atividade values('','Login em Cliente');
+insert into Tipo_Atividade values('','Login no Servidor');
+insert into Tipo_Atividade values('','Pontução Básica');
 
---Tabela Configuracao
-insert into Configuracao values('','Easy',1)
-insert into Configuracao values('','Medium',2)
-insert into Configuracao values('','Hard',3)
+-- Tabela Configuracao
+insert into Configuracao values('','Easy',1);
+insert into Configuracao values('','Medium',2);
+insert into Configuracao values('','Hard',3);
