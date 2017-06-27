851032e5d9a23ba00d0f8cc16464977688551722
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:00:54 -0300

    AplTioNave criada a configuração na camada de Gerência de Tarefa

---
+++ _SERVIDOR/viagemestelar/app/cgt/AplTipoNave.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\cgt;
+
+/**
+ * Description of AplTipoNave
+ *
+ * @author pchan
+ */
+class AplTipoNave {
+    //put your code here
+}
