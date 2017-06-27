2be0c3bd80eea64d91d24e909985f96d9e89a723
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:16:53 -0300

    AplServico removido da camada de Gerência de Tarefa

--- _SERVIDOR/viagemestelar/app/cgt/AplPartida.php
+++ _SERVIDOR/viagemestelar/app/cgt/AplPartida.php
@@ -8,11 +8,34 @@
 
 namespace app\cgt;
 
+use app\cgd\PartidaDao;
+use app\cgt\InterfaceDeApresentacao;
 /**
  * Description of AplPartida
  *
  * @author pchan
  */
-class AplPartida {
-    //put your code here
+class AplPartida implements InterfaceDeApresentacao {
+    
+    private $partidaDao;
+    
+    public function __construct() {
+        $this->partidaDao = new PartidaDao(); 
+    }
+    
+    public function alterar($objeto): bool {
+        return $this->partidaDao->alterar($objeto);
+    }
+
+    public function deletar($id): bool {
+        return $this->partidaDao->delete($id);
+    }
+
+    public function find($id) {
+        return $this->partidaDao->find($id);
+    }
+
+    public function listar($ordem): array {
+        return $this->partidaDao->listar($ordem);
+    }
 }
