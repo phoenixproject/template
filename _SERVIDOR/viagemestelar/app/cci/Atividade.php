1d385c415dd0b45a04cc49daf45c61e99a3ae150
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:48:50 -0300

    Finalizada a configuração da classe Atividade na camada de controle de interface

--- _SERVIDOR/viagemestelar/app/cci/Atividade.php
+++ _SERVIDOR/viagemestelar/app/cci/Atividade.php
@@ -8,11 +8,87 @@
 
 namespace app\cci;
 
+use app\cgt\AplUsuario;
+use app\cgt\AplTipoAtividade;
+use app\cgt\InterfaceDeApresentacao;
+
+use app\cdp\Atividade as AtividadeDominio;
+
+use ifes\controller\Action;
 /**
  * Description of Atividade
  *
  * @author pchan
  */
-class Atividade {
-    //put your code here
+class Atividade extends Action {
+    
+    private $interfaceDeApresentacaoUsuario;
+    private $interfaceDeApresentacaoTipoAtividade;
+    private $interfaceDeApresentacaoAtividade;
+    
+    public function __construct() {
+        $this->interfaceDeApresentacaoUsuario = new AplUsuario();
+        $this->interfaceDeApresentacaoTipoAtividade = new AplTipoAtividade();
+        $this->interfaceDeApresentacaoAtividade = new AplAtividade();
+    }
+    
+    public function retrieve(){
+        $this->render('retrieve',false);
+    }
+    
+    public function getall(){
+        $this->render('getall',false);        
+    }
+    
+    public function add(){
+        $this->render('add',false);        
+    }
+    
+    public function edit(){
+        $this->render('edit',false);        
+    }
+    
+    public function listarAtividade($ordem){
+        return $this->interfaceDeApresentacaoUsuario->listar($ordem);
+    }
+    
+    public function listarTipoDeAtividade($ordem){
+        return $this->interfaceDeApresentacaoTipoAtividade->listar($ordem);
+    }
+    
+    public function listarUsuario($ordem){
+        return $this->interfaceDeApresentacaoUsuario->listar($ordem);
+    }
+    
+    public function findById(){
+        
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if(isset($id)){            
+            return $this->interfaceDeApresentacaoUsuario->find($id);
+        }
+        return "";        
+    }
+    
+    public function save(){
+        
+        $atividade = filter_input(INPUT_POST, 'atividade', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if(isset($atividade))
+        {
+            $atividadeDominio = new AtividadeDominio();            
+                        
+            $chave = array_values($atividade);
+                        
+            $atividadeDominio->setCd_atividade($chave[0]);
+            $atividadeDominio->setDt_atividade($chave[1]);
+            $atividadeDominio->setCd_usuario($chave[2]);
+            $atividadeDominio->setTp_atividade($chave[3]);
+                                    
+            if($this->interfaceDeApresentacaoAtividade->alterar($atividadeDominio)){
+                $this->render('getall',false);        
+            }
+        }    
+        return "";                
+    }    
 }
