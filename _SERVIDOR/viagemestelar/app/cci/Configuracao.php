2e4b38410fdfc7065a0c09846a9bbcef20f2a0fa
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:50:36 -0300

    Criada a configuração da classe Configuracao na camada de controle de interface

---
+++ _SERVIDOR/viagemestelar/app/cci/Configuracao.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\cci;
+
+/**
+ * Description of Configuracao
+ *
+ * @author pchan
+ */
+class Configuracao {
+    //put your code here
+}
