f13de34f86e4273ca7513e0a02450e20b55a26d9
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:56:49 -0300

    Finalizada a configuração da classe Configuracao na camada de controle de interface

--- _SERVIDOR/viagemestelar/app/cci/Configuracao.php
+++ _SERVIDOR/viagemestelar/app/cci/Configuracao.php
@@ -8,11 +8,72 @@
 
 namespace app\cci;
 
+use app\cgt\InterfaceDeApresentacao;
+
+use app\cdp\Configuracao as ConfiguracaoDominio;
+
+use ifes\controller\Action;
 /**
  * Description of Configuracao
  *
  * @author pchan
  */
-class Configuracao {
-    //put your code here
+class Configuracao extends Action {
+    
+    private $interfaceDeApresentacao;
+        
+    public function __construct() {
+        $this->interfaceDeApresentacao = new AplConfiguracao();       
+    }
+    
+    public function retrieve(){
+        $this->render('retrieve',false);
+    }
+    
+    public function getall(){
+        $this->render('getall',false);        
+    }
+    
+    public function add(){
+        $this->render('add',false);        
+    }
+    
+    public function edit(){
+        $this->render('edit',false);        
+    }
+    
+    public function listar($ordem){
+        return $this->interfaceDeApresentacao->listar($ordem);
+    }
+    
+    public function findById(){
+        
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if(isset($id)){            
+            return $this->interfaceDeApresentacao->find($id);
+        }
+        return "";        
+    }
+    
+    public function save(){
+        
+        $configuracao = filter_input(INPUT_POST, 'configuracao', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if(isset($configuracao))
+        {
+            $configuracaoDominio = new ConfiguracaoDominio();            
+                        
+            $chave = array_values($configuracao);
+                        
+            $configuracaoDominio->setCd_configuracao($chave[0]);
+            $configuracaoDominio->setDs_configuracao($chave[1]);
+            $configuracaoDominio->setNivel_dificuldade($chave[2]);
+                                                
+            if($this->interfaceDeApresentacao->alterar($configuracaoDominio)){
+                $this->render('getall',false);        
+            }
+        }    
+        return "";                
+    }    
 }
