189f65c87724f91fd460bbdffaeb9c7183097550
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:03:59 -0300

    AplTipoAtividade finalizada a configuração na camada de Gerência de Tarefa

--- _SERVIDOR/viagemestelar/app/cgt/AplTipoAtividade.php
+++ _SERVIDOR/viagemestelar/app/cgt/AplTipoAtividade.php
@@ -8,11 +8,34 @@
 
 namespace app\cgt;
 
+use app\cgd\TipoAtividadeDao;
+use app\cgt\InterfaceDeApresentacao;
 /**
  * Description of AplTipoAtividade
  *
  * @author pchan
  */
-class AplTipoAtividade {
-    //put your code here
+class AplTipoAtividade  implements InterfaceDeApresentacao {
+    
+    private $tipoAtividadeDao;
+    
+    public function __construct() {
+        $this->tipoAtividadeDao = new TipoAtividadeDao(); 
+    }
+    
+    public function alterar($objeto): bool {
+        return $this->tipoAtividadeDao->alterar($objeto);
+    }
+
+    public function deletar($id): bool {
+        return $this->tipoAtividadeDao->delete($id);
+    }
+
+    public function find($id) {
+        return $this->tipoAtividadeDao->find($id);
+    }
+
+    public function listar($ordem): array {
+        return $this->tipoAtividadeDao->listar($ordem);
+    }
 }
