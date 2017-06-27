40aae98a917c99d4f54bbc8d7983b867a5525317
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 14:24:26 -0300

    Finalizada a configuração da classe Tipousuario na camada de controle de interface

--- _SERVIDOR/viagemestelar/app/cci/Tipousuario.php
+++ _SERVIDOR/viagemestelar/app/cci/Tipousuario.php
@@ -8,11 +8,74 @@
 
 namespace app\cci;
 
+use app\cgt\AplTipoUsuario;
+use app\cgt\InterfaceDeApresentacao;
+
+use app\cdp\TipoUsuario;
+
+use ifes\controller\Action;
 /**
  * Description of Tipousuario
  *
  * @author pchan
  */
-class Tipousuario {
-    //put your code here
+class Tipousuario extends Action {
+        
+    private $interfaceDeApresentacao;
+    
+    public function __construct() {        
+        $this->interfaceDeApresentacao = new AplTipoUsuario();
+    }
+    
+    public function retrieve(){
+        $this->render('retrieve',false);
+    }
+    
+    public function getall(){
+        $this->render('getall',false);        
+    }
+    
+    public function add(){
+        $this->render('add',false);        
+    }
+    
+    public function edit(){
+        $this->render('edit',false);        
+    }
+    
+    public function listar($ordem){
+        return $this->interfaceDeApresentacao->listar($ordem);
+    }
+    
+    public function findById(){
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+            return $this->interfaceDeApresentacao->find($id);
+        }
+        return "";        
+    }
+    
+    public function listarTipoUsuario($ordem){
+        return $this->interfaceDeApresentacao->listar($ordem);
+    }
+    
+    public function save(){
+            
+        if(isset($_POST['tipousuario']))
+        {
+            $tipoUsuarioDominio = new TipoUsuario();
+            
+            $tipoUsuario = $_POST['tipousuario'];            
+                        
+            $chave = array_values($tipoUsuario);
+                        
+            $tipoUsuarioDominio->setTp_usuario($chave[0]);
+            $tipoUsuarioDominio->setDs_tp_usuario($chave[1]);
+                                    
+            if($this->interfaceDeApresentacaoUsuario->alterar($tipoUsuarioDominio)){
+                $this->render('getall',false);        
+            }
+        }    
+        return "";                
+    }
 }
