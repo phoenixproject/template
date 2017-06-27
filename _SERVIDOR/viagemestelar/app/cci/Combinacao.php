b956d7bd5681741209f10e9a0b32f6f2227feb26
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 15:17:21 -0300

    Finalizada a configuração da classe Combinacao na camada de controle de interface

--- _SERVIDOR/viagemestelar/app/cci/Combinacao.php
+++ _SERVIDOR/viagemestelar/app/cci/Combinacao.php
@@ -8,11 +8,87 @@
 
 namespace app\cci;
 
+use app\cgt\AplTipoNave;
+use app\cgt\AplTipoArma;
+use app\cgt\InterfaceDeApresentacao;
+
+use app\cdp\Combinacao as CombinacaoDominio;
+
+use ifes\controller\Action;
 /**
  * Description of Combinacao
  *
  * @author pchan
  */
-class Combinacao {
-    //put your code here
+class Combinacao extends Action {
+    
+    private $interfaceDeApresentacaoTipoNave;
+    private $interfaceDeApresentacaoTipoArma;
+    private $interfaceDeApresentacaoCombinacao;
+    
+    public function __construct() {
+        $this->interfaceDeApresentacaoTipoNave = new AplTipoNave();
+        $this->interfaceDeApresentacaoTipoArma = new AplTipoArma();
+        $this->interfaceDeApresentacaoCombinacao = new AplCombinacao();
+    }
+    
+    public function retrieve(){
+        $this->render('retrieve',false);
+    }
+    
+    public function getall(){
+        $this->render('getall',false);        
+    }
+    
+    public function add(){
+        $this->render('add',false);        
+    }
+    
+    public function edit(){
+        $this->render('edit',false);        
+    }
+    
+    public function listarTipoDeNave($ordem){
+        return $this->interfaceDeApresentacaoTipoNave->listar($ordem);
+    }
+    
+    public function listarTipoDeArma($ordem){
+        return $this->interfaceDeApresentacaoTipoArma->listar($ordem);
+    }
+    
+    public function listarCombinacao($ordem){
+        return $this->interfaceDeApresentacaoCombinacao->listar($ordem);
+    }
+    
+    public function findById(){
+        
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if(isset($id)){            
+            return $this->interfaceDeApresentacaoCombinacao->find($id);
+        }
+        return "";        
+    }
+    
+    public function save(){
+        
+        $combinacao = filter_input(INPUT_POST, 'combinacao', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if(isset($combinacao))
+        {
+            $combinacaoDominio = new CombinacaoDominio();            
+                        
+            $chave = array_values($combinacao);
+                        
+            $combinacaoDominio->setCd_combinacao($chave[0]);
+            $combinacaoDominio->setDs_combinacao($chave[1]);
+            $combinacaoDominio->setTp_nave($chave[2]);
+            $combinacaoDominio->setTp_arma($chave[3]);
+                                    
+            if($this->interfaceDeApresentacaoCombinacao->alterar($combinacaoDominio)){
+                $this->render('getall',false);        
+            }
+        }    
+        return "";                
+    }    
 }
