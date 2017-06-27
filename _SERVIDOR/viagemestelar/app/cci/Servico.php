41c984d1842ef22b493518be0e40d92195427d32
Phoenix Project <phoenixproject.erp@gmail.com>
2017-06-27 15:33:27 -0300

    Recriada a classe Servico na camada de controle de interface

---
+++ _SERVIDOR/viagemestelar/app/cci/Servico.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\cci;
+use app\cgt\InterfaceDeServico;
+use app\cgt\AplServico;
+/**
+ * Description of Servico
+ *
+ * @author pchan
+ */
+class Servico {
+    
+    private $interfaceDeServico;
+    
+    public function __construct() {
+        $this->interfaceDeServico = new AplServico();
+    }
+ 
+}
