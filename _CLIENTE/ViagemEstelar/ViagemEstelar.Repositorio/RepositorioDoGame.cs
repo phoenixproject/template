8cde799782f90294678fe76bf2306fa194fb2710
Phoenix Project <phoenixproject.erp@gmail.com>
2017-07-05 08:28:15 -0300

    Diagramas de Padrão de Projeto adicionados

--- _CLIENTE/ViagemEstelar/ViagemEstelar.Repositorio/RepositorioDoGame.cs
+++ _CLIENTE/ViagemEstelar/ViagemEstelar.Repositorio/RepositorioDoGame.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace ViagemEstelar.Repositorio
-{
+{	
 	public class RepositorioDoGame
 	{
+		private static readonly HttpClient client = new HttpClient();
+
+
 	}
 }
